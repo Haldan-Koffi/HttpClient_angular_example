ca166c40fed93290052e33e4b7f58132f7ad3d62
Haldan Koffi <e.haldankoffi@gmail.com>
2024-12-20 16:43:06 +0100

    :feat: Ajout de HttpClient, requête d'une API et affichage dans la template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +7,28 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'projet_test';
+export class AppComponent implements OnInit {
+  articles: any[] = [];
+  errorMessage: string | null = null;
+  private apiUrl = 'https://fakestoreapi.com/products'; // URL de l'API
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadArticles();
+  }
+
+  loadArticles(): void {
+    this.http.get<any[]>(this.apiUrl).subscribe({
+      next: (data) => {
+        this.articles = data;
+      },
+      error: (error) => {
+        console.error('Erreur lors du chargement des articles :', error);
+        this.errorMessage = 'Impossible de charger les articles.';
+      },
+    });
+  }
 }
